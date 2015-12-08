29b7f1894f3e6b4011fc1b55f801ef0fab9eeef1
ahm04 <ahm04@hotmail.co.uk>
2015-12-08 17:17:21 +0000

    Update brain
    Check file and load

---
+++ Note.java
@@ -0,0 +1,24 @@
+package com.wellbeings.anatome;
+
+/**
+ * Created by Calum & Ahm on 29/11/2015.
+ */
+public class Note {
+    //string storing the note's content
+    private String content;
+    public String getContent(){
+        return new String(content);
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    //String storing the note's creation date
+    private String creationDate;
+    public String getCreationDate(){
+        return new String(creationDate);
+    }
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+}
