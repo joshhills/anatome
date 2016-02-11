be2a36281033881a124d07a5c6eb5a5e626b6533
Lizzie Lovell <erj.lovell@gmail.com>
2016-02-11 22:13:23 +0000

    Created the Heart Fragment
    Moved the heart activity into a fragment
    Deleted the old heart activity as no longer needed

--- anatome/app/src/main/java/io/wellbeings/anatome/HeartWidget.java
+++ anatome/app/src/main/java/io/wellbeings/anatome/HeartWidget.java
@@ -7,12 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HeartWidget extends Fragment implements Widget {
+public class HeartWidget extends Fragment implements Widget, View.OnClickListener {
 
     public HeartWidget() {
         // Required empty public constructor
@@ -33,7 +36,52 @@ public class HeartWidget extends Fragment implements Widget {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_heart_widget, container, false);
+        View v = inflater.inflate(R.layout.fragment_heart_widget, container, false);
+
+        // Set up the button listeners
+        Button startButton = (Button) v.findViewById(R.id.buttonStart);
+        startButton.setOnClickListener(this);
+
+        Button stopButton = (Button) v.findViewById(R.id.buttonStop);
+        stopButton.setOnClickListener(this);
+
+        return v;
     }
 
+    public void startButtonOnClick(View v) {
+
+        View circleView = (View) getView().findViewById(R.id.circleView);
+        Animation circleAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.circle_animation);
+        circleAnimation.setRepeatCount(Animation.INFINITE);
+        circleView.startAnimation(circleAnimation);
+    }
+
+    public void stopButtonOnClick(View v) {
+
+        View circleView = (View) getView().findViewById(R.id.circleView);
+
+        Animation circleAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.stop_animation);
+        circleAnimation.setRepeatCount(Animation.INFINITE);
+        circleView.startAnimation(circleAnimation);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonStart:
+
+                startButtonOnClick(v);
+
+
+                break;
+
+            case R.id.buttonStop:
+
+                stopButtonOnClick(v);
+
+                break;
+        }
+    }
 }
