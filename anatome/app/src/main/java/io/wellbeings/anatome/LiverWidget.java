f762f9273e96f67e8e89fa7fa5f6c9c4b4ca096a
Josh Hills <josh.hills@ymail.com>
2016-02-21 18:40:32 +0000

    LiverWidget Development
    Have retrieved spinners and added first initialisation methods.

--- anatome/app/src/main/java/io/wellbeings/anatome/LiverWidget.java
+++ anatome/app/src/main/java/io/wellbeings/anatome/LiverWidget.java
@@ -7,6 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,16 +20,17 @@ import android.view.ViewGroup;
  */
 public class LiverWidget extends Fragment implements Widget {
 
+    /* Useful private variables. */
+
+    // Store name of section.
+    private final String SECTION = "liver";
+    // Store view object for UI manipulation.
     private View v;
 
-    public LiverWidget() {
-        // Required empty public constructor
-    }
-
+    // Necessary empty creation methods.
+    public LiverWidget() {}
     public static LiverWidget newInstance() {
-        LiverWidget fragment = new LiverWidget();
-        Bundle args = new Bundle();
-        return fragment;
+        return new LiverWidget();
     }
 
     @Override
@@ -34,9 +41,53 @@ public class LiverWidget extends Fragment implements Widget {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        // Inflate the layout for this fragment, storing view.
         v = inflater.inflate(R.layout.fragment_liver_widget, container, false);
         return v;
+    }
+
+    // Populate spinners with related information.
+    private void initSpinners() {
+
+        // Retrieve references to spinners.
+        Spinner drinkSpinner = (Spinner) v.findViewById(R.id.drinkSpinner);
+        Spinner volumeSpinner = (Spinner) v.findViewById(R.id.volumeSpinner);
+        Spinner percentageSpinner = (Spinner) v.findViewById(R.id.percentageSpinner);
+
+        /* Populate spinner options with correct localization. */
+
+        // Drink spinner listener (one instance, anonymous).
+        drinkSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    // TODO: Phil, fill case statements appropriately - have commented example.
+                    case 0:
+                        // volumeSpinner.setSelection(int);
+                        break;
+                }
+            }
+        });
+        ArrayAdapter<String> drinkAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item,
+                UtilityManager.getContentLoader(getContext()).getInfoTextAsList(SECTION, "drinks", ","));
+        // Set style.
+        drinkAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Attach adapter.
+        drinkSpinner.setAdapter(drinkAdapter);
+
+        // Volume spinner listener (one instance, anonymous).
+        volumeSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    // TODO: Phil, fill case statements appropriately - have commented example.
+                    case 0:
+                        // Perform conversion and set name.
+                }
+            }
+        });
+
     }
 
 }
