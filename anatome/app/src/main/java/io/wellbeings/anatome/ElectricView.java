75042f9de1bd74cc14c94077a3cdd5bb2f70998e
Lizzie Lovell <erj.lovell@gmail.com>
2016-04-18 21:54:55 +0100

    Modified ElectricView Class
    Changed to use constants, removed magical numbers
    Ensured all code was commented

--- anatome/app/src/main/java/io/wellbeings/anatome/ElectricView.java
+++ anatome/app/src/main/java/io/wellbeings/anatome/ElectricView.java
@@ -10,6 +10,7 @@ import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.RadialGradient;
 import android.graphics.Shader;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -23,13 +24,28 @@ import java.util.Random;
  * Purpose: A class to provide an electrical animation for the brain within the main scroll
  */
 public class ElectricView extends View {
+    
+    // constant values for line drawing calculations
+    private static final int FRAMES_BEFORE_DRAW = 4;
+    private static final int DELAY_BETWEEN_BOLTS = 100;
+    private static final int MAX_POINTS = 8;
+    private static final int RANDOM_POINT_RANGE = 100;
+    private static final int SEGMENT_LENGTH = 40;
+    private static final int MAX_POINTS_BEFORE_FORK = 6;
+    private static final int FORK_POINT_INDEX = 3;
+    private static final int LINE_WIDTH = 10;
 
+    // painting context for line
     Paint paint;
-    Path path;
+    // store current points
     List<Point> points = new ArrayList<Point>();
+    // current direction of line
     Point direction;
 
-
+    /**
+     * Overload constructors of the View Class,
+     * it is necessary to run the init method on each instance
+     * **/
     public ElectricView(Context context) {
         super(context);
         init();
@@ -45,138 +61,140 @@ public class ElectricView extends View {
         init();
     }
 
+    /**
+     * set styles of the lines
+     */
     private void init(){
+       
+       // load lightning bolt colour 
+       int lightningBoltColour = ContextCompat.getColor(this.getContext(), R.color.electric_bolt );
+
+        // create new paint and set rendering style
         paint = new Paint();
-
+        // drawing lines so render style is stroke
         paint.setStyle(Paint.Style.STROKE);
-
+        // set colour of line
+        paint.setColor(lightningBoltColour);
+        // set width of line
+        paint.setStrokeWidth(LINE_WIDTH);
+        // set styling of the line
+        paint.setShader(new RadialGradient(8f, 80f, 30f, lightningBoltColour , Color.WHITE, Shader.TileMode.MIRROR));
     }
 
-    // generate random number
+    // create random number generator
+    // used for random angles and position of lightning
     Random random  = new Random();
 
+    // store current frame number
+    int currentFrame;
+    // store next frame number to start drawing lightning bolt
     int nextDrawFrame;
-    int i;
+
+
+    /**
+     * Logic for drawing lightning bolt
+      * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
-        i++;
-
+        currentFrame++;
+        
         super.onDraw(canvas);
 
-        // limit the number of times per second
-        if ( i%4 ==0 && i > nextDrawFrame) {
+        // set the frames per second
+        if ( currentFrame % FRAMES_BEFORE_DRAW == 0 && currentFrame > nextDrawFrame) {
 
-            if (points.size() > 8) {
+            // check lightning bolt is complete?
+            if (points.size() > MAX_POINTS) {
+                // start a new lightning bolt
                 points.clear();
-                nextDrawFrame = nextDrawFrame + 100;
-
-
+                // add a delay
+                nextDrawFrame = nextDrawFrame + DELAY_BETWEEN_BOLTS;
             }
 
-            if (points.size()== 0 && i > nextDrawFrame) {
+            // create the first point if no points exist
+            if (points.size()== 0 && currentFrame > nextDrawFrame) {
 
-                int randomx = random.nextInt(100) - 50;
-                int randomy = random.nextInt(100) - 50;
+                // generate numbers for a random centre point offset
+                int randomx = random.nextInt(RANDOM_POINT_RANGE) - RANDOM_POINT_RANGE/2;
+                int randomy = random.nextInt(RANDOM_POINT_RANGE) - RANDOM_POINT_RANGE/2;
 
-                // find the center point
+                // find the center point, and add the random offset
                 Point start = new Point(this.getMeasuredWidth()/2 + randomx,  this.getMeasuredHeight()/4 + randomy);
 
+                // add the starting position to the points
                 points.add(start);
 
-                // calculate random angle
-                int a = random.nextInt(360);
+                // calculate a random angle for the direction
+                int angle = random.nextInt(360);
 
+                // calculate x and y components of random angle
+                int x = (int) (Math.cos(angle) * SEGMENT_LENGTH);
+                int y = (int) ( Math.sin(angle) * SEGMENT_LENGTH);
 
-                int y = (int) ( Math.sin(a) * 40);
-
-                int x = (int) (Math.cos(a) * 40);
-
+                // store the initial direction of the lightning bolt
                 direction  = new Point(x,y);
 
-                // calculate the direction of the next electric bolt
+                // add new direction to the random start point
                 Point point = new Point(x + start.x,y + start.y);
+                // add to the points
                 points.add(point);
-
-
             }
 
-
+                // check if there is a start point
                 if (points.size() > 0) {
 
-                    // generate a random number to use the the next point
-                    int x = random.nextInt(100) - 50;
-                    int y = random.nextInt(100) - 50;
+                    // generate numbers for a random centre point offset
+                    int x = random.nextInt(RANDOM_POINT_RANGE) - RANDOM_POINT_RANGE/2;
+                    int y = random.nextInt(RANDOM_POINT_RANGE) - RANDOM_POINT_RANGE/2;
 
-                    // get last point in the list
+                    // get the last point drawn
                     Point lastPoint = points.get(points.size() - 1);
 
+                    // check if ready to fork point
+                    if (points.size() == MAX_POINTS_BEFORE_FORK) {
 
-                    if (points.size() == 6) {
-
-                        lastPoint = points.get(points.size() - 3);
+                        // set last point to be start of fork
+                        lastPoint = points.get(points.size() - FORK_POINT_INDEX);
 
                     }
 
-                    // calculate the end point of the line
+                    // add new direction and random offset to the previous point
                     Point point = new Point(x + direction.x + lastPoint.x, y + direction.y + lastPoint.y);
 
-                    // add the the list of points
+                    // add to the points
                     points.add(point);
-
                 }
-
-
-
-
-
-
         }
 
 
-
-
-
-        // check there is a start and finish point
+        // check there is at least two points to draw the line
         if (points.size() >= 2) {
 
-
-            path = new Path();
-            paint.setColor(Color.parseColor("#cefffb"));
-            paint.setStrokeWidth(10);
-            paint.setShader(new RadialGradient(8f, 80f, 30f, Color.parseColor("#cefffb"), Color.WHITE, Shader.TileMode.MIRROR));
-            paint.setMaskFilter(new BlurMaskFilter(30, BlurMaskFilter.Blur.OUTER));
-            paint.setMaskFilter(new BlurMaskFilter(15, BlurMaskFilter.Blur.SOLID));
+            // create a new drawing path
+           Path path = new Path();
+            // set the start position to the first point
             path.moveTo(points.get(0).x, points.get(0).y);
 
-
-            int i = 0;
-
-
            // loop through points, draw a line to each
-            for (Point j : points) {
+            for (int i = 0; i < points.size(); i++){
 
+                //check for fork
+                if (i == MAX_POINTS_BEFORE_FORK) {
 
-                if (i == 6) {
-
-                    path.moveTo(points.get(3).x, points.get(3).y);
+                    // move the path to the fork start, without drawing
+                    path.moveTo(points.get(FORK_POINT_INDEX).x, points.get(FORK_POINT_INDEX).y);
                 }
 
-                path.lineTo(j.x, j.y);
-
-                i++;
+                // draw to the next point
+                path.lineTo(points.get(i).x, points.get(i).y);
             }
 
+            // draw the path on the canvas
             canvas.drawPath(path, paint);
-
         }
-
-
+        // redrawn immediately
         this.invalidate();
-
-
-
     }
-
-
 }
 
