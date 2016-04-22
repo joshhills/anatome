15e8cff7ca10da1baeb7970b12d5730dd2373542
calummackervoy <c.mackervoy@gmail.com>
2016-04-22 18:17:44 +0100

    Resolved conflict

--- anatome/app/src/main/java/io/wellbeings/anatome/AudioManager.java
+++ anatome/app/src/main/java/io/wellbeings/anatome/AudioManager.java
@@ -34,7 +34,6 @@ public final class AudioManager {
         // return songslist
         return songsList;
 
-<<<<<<< HEAD
     }
 
     //play the audio from a note object
@@ -67,8 +66,6 @@ public final class AudioManager {
         if(mp.isPlaying()){
             mp.stop();
         }
-=======
->>>>>>> origin/master
     }
 
 }
