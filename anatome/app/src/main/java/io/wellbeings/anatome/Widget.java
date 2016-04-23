8160839346c0898dadd218b959b892d219cd41ec
Josh Hills <josh.hills@ymail.com>
2016-04-23 21:35:24 +0100

    Extra Alcohol Threshold, Expanded Widget Interface
    Content now more sensible, also removed class implementations of
    ‘Widget’ until they can be added safely.

--- anatome/app/src/main/java/io/wellbeings/anatome/Widget.java
+++ anatome/app/src/main/java/io/wellbeings/anatome/Widget.java
@@ -2,8 +2,22 @@ package io.wellbeings.anatome;
 
 /**
  * Interface ensures semantic link
- * between interactive sub-sections.
+ * between interactive sub-sections,
+ * enforces a more coherent MVC pattern
+ * within classes.
  */
-public interface Widget {
+interface Widget {
+
+    /**
+     * Initialise any visual elements with the correct
+     * graphics and text.
+     */
+    void initGUI();
+
+    /**
+     * Add any event-driven functionality, which
+     * is usually messy within code.
+     */
+    void attachListeners();
 
 }
