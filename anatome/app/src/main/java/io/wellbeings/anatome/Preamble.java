421346f6c6eb5802c565f296a99875ba77b0a7e8
Josh Hills <josh.hills@ymail.com>
2016-04-24 14:45:12 +0100

    Altered Preamble
    Lock screen navigation no longer hidden.

--- anatome/app/src/main/java/io/wellbeings/anatome/Preamble.java
+++ anatome/app/src/main/java/io/wellbeings/anatome/Preamble.java
@@ -1,16 +1,13 @@
 package io.wellbeings.anatome;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.io.IOException;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -32,13 +29,6 @@ public class Preamble extends AppCompatActivity {
         // Hide intrusive android status bars.
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         super.onCreate(savedInstanceState);
 
